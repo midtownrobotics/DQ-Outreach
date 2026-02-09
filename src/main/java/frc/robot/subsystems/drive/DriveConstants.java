2f89eb8d5e1ded0055d06de100aa05221d4b870a
Gray486 <me@grayjn.com>
2026-02-09 17:17:14 -0500

    initial commit

---
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -0,0 +1,123 @@
+// Copyright (c) 2021-2026 Littleton Robotics
+// http://github.com/Mechanical-Advantage
+//
+// Use of this source code is governed by a BSD
+// license that can be found in the LICENSE file
+// at the root directory of this project.
+
+package frc.robot.subsystems.drive;
+
+import com.ctre.phoenix6.CANBus;
+import com.pathplanner.lib.config.ModuleConfig;
+import com.pathplanner.lib.config.RobotConfig;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+
+public class DriveConstants {
+  public static final double maxSpeedMetersPerSec = 4.8;
+  public static final double odometryFrequency = 100.0; // Hz
+  public static final double trackWidth = Units.inchesToMeters(15.25);
+  public static final double wheelBase = Units.inchesToMeters(16.25);
+  public static final double driveBaseRadius = Math.hypot(trackWidth / 2.0, wheelBase / 2.0);
+  public static final Translation2d[] moduleTranslations =
+      new Translation2d[] {
+        new Translation2d(trackWidth / 2.0, wheelBase / 2.0),
+        new Translation2d(trackWidth / 2.0, -wheelBase / 2.0),
+        new Translation2d(-trackWidth / 2.0, wheelBase / 2.0),
+        new Translation2d(-trackWidth / 2.0, -wheelBase / 2.0)
+      };
+
+  // Zeroed rotation values for each module, see setup instructions
+  //   public static final Rotation2d frontLeftZeroRotation = new Rotation2d(-1.349);
+  //   public static final Rotation2d frontRightZeroRotation = new Rotation2d(-2.377);
+  //   public static final Rotation2d backLeftZeroRotation = new Rotation2d(2.636);
+  //   public static final Rotation2d backRightZeroRotation = new Rotation2d(0.11);
+
+  public static final Rotation2d frontLeftZeroRotation = new Rotation2d(1.310);
+  public static final Rotation2d frontRightZeroRotation = new Rotation2d(2.373);
+  public static final Rotation2d backLeftZeroRotation = new Rotation2d(-2.636);
+  public static final Rotation2d backRightZeroRotation = new Rotation2d(-0.13);
+
+  public static final CANBus ctreCanBus = new CANBus("Sensors");
+
+  // Device CAN IDs
+  public static final int pigeonCanId = 55;
+
+  public static final int frontLeftDriveCanId = 52;
+  public static final int backLeftDriveCanId = 42;
+  public static final int frontRightDriveCanId = 22;
+  public static final int backRightDriveCanId = 32;
+
+  public static final int frontLeftTurnCanId = 51;
+  public static final int backLeftTurnCanId = 41;
+  public static final int frontRightTurnCanId = 21;
+  public static final int backRightTurnCanId = 31;
+  public static final int frontLeftEncoderCanId = 50;
+  public static final int backLeftEncoderCanId = 40;
+  public static final int frontRightEncoderCanId = 20;
+  public static final int backRightEncoderCanId = 30;
+
+  // Drive motor configuration
+  public static final int driveMotorCurrentLimit = 50;
+  public static final double wheelRadiusMeters = Units.inchesToMeters(2);
+  public static final double driveMotorReduction = 6.75; // Mk4i L2
+  public static final DCMotor driveGearbox = DCMotor.getNeoVortex(1);
+
+  // Drive encoder configuration
+  public static final double driveEncoderPositionFactor =
+      2 * Math.PI / driveMotorReduction; // Rotor Rotations ->
+  // Wheel Radians
+  public static final double driveEncoderVelocityFactor =
+      (2 * Math.PI) / 60.0 / driveMotorReduction; // Rotor RPM ->
+  // Wheel Rad/Sec
+
+  // Drive PID configuration
+  public static final double driveKp = 0.0;
+  public static final double driveKd = 0.0;
+  public static final double driveKs = 0.0;
+  public static final double driveKv = 0.1;
+  public static final double driveSimP = 0.05;
+  public static final double driveSimD = 0.0;
+  public static final double driveSimKs = 0.0;
+  public static final double driveSimKv = 0.0789;
+
+  // Turn motor configuration
+  public static final boolean turnInverted = true;
+  public static final int turnMotorCurrentLimit = 20;
+  public static final double turnMotorReduction = 21.45; // Mk4i L2
+  public static final DCMotor turnGearbox = DCMotor.getNeo550(1);
+
+  // Turn encoder configuration
+  public static final boolean turnEncoderInverted = true;
+  public static final double turnEncoderPositionFactor =
+      2 * Math.PI / turnMotorReduction; // Rotor Rotations -> Wheel Radians
+  public static final double turnEncoderVelocityFactor =
+      (2 * Math.PI) / 60.0 / turnMotorReduction; // Rotor RPM -> Wheel Rad/Sec
+
+  // Turn PID configuration
+  public static final double turnKp = 2.0;
+  public static final double turnKd = 0.0;
+  public static final double turnSimP = 8.0;
+  public static final double turnSimD = 0.0;
+  public static final double turnPIDMinInput = 0; // Radians
+  public static final double turnPIDMaxInput = 2 * Math.PI; // Radians
+
+  // PathPlanner configuration
+  public static final double robotMassKg = 74.088;
+  public static final double robotMOI = 6.883;
+  public static final double wheelCOF = 1.2;
+  public static final RobotConfig ppConfig =
+      new RobotConfig(
+          robotMassKg,
+          robotMOI,
+          new ModuleConfig(
+              wheelRadiusMeters,
+              maxSpeedMetersPerSec,
+              wheelCOF,
+              driveGearbox.withReduction(driveMotorReduction),
+              driveMotorCurrentLimit,
+              1),
+          moduleTranslations);
+}
